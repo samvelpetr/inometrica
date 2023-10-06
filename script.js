76fb6b989ad13555522c672f3004863702e821b7
samvelpetr <petrosans05@gmail.com>
2023-10-07 03:04:14 +0400

    commiting

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const swiper1 = new Swiper('.swiper1', {
     slidesPerView: 1,
-   
+
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -13,7 +13,9 @@ const swiper1 = new Swiper('.swiper1', {
             return `<span class = "` + className + `">` + `<p>` + `0` + (index + 1) + `</p>` + `</span>`
         }
     },
-    direction: 'vertical'
+    direction: 'vertical',
+    loop: true
+
 })
 
 const swiper2 = new Swiper('.swiper2', {
@@ -22,15 +24,17 @@ const swiper2 = new Swiper('.swiper2', {
         nextEl: '.swiper-button-next1',
         prevEl: '.swiper-button-prev1',
     },
+    loop: true
+
 })
 
 const tool__element = document.querySelectorAll('.tool__element');
 
 const tool__element_open = document.querySelectorAll('.tool__element_open')
 
-const cases__button =document.querySelectorAll('.cases__button')
+const cases__button = document.querySelectorAll('.cases__button')
 
-const cases__element_main=document.querySelectorAll('.cases__element_main')
+const cases__element_main = document.querySelectorAll('.cases__element_main')
 tool__element.forEach((e, i) => {
     e.addEventListener('click', function () {
 
@@ -56,8 +60,8 @@ cases__button.forEach((e, i) => {
     })
 })
 
-const swiper3 =new Swiper('.swiper3',{
-    slidesPerView:1,
+const swiper3 = new Swiper('.swiper3', {
+    slidesPerView: 1,
 
     navigation: {
         nextEl: '.swiper-button-next2',
@@ -66,7 +70,9 @@ const swiper3 =new Swiper('.swiper3',{
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
-    }
+    },
+    loop: true
+
 })
 
 const swiper4 = new Swiper('.swiper4', {
@@ -75,13 +81,81 @@ const swiper4 = new Swiper('.swiper4', {
         nextEl: '.swiper-button-next3',
         prevEl: '.swiper-button-prev3',
     },
-    spaceBetween:24
+    spaceBetween: 24,
+    loop: true
 })
 
-const swiper5 = new Swiper('.swiper5',{
+const swiper5 = new Swiper('.swiper5', {
     slidesPerView: 5,
     navigation: {
         nextEl: '.swiper-button-next5',
         prevEl: '.swiper-button-prev5',
     },
+    loop: true
+
+})
+
+
+
+const checkedBut = document.querySelectorAll('.checkedBut');
+
+const inputCheck = document.querySelectorAll('.inputCheck input');
+
+inputCheck.forEach((e, i) => {
+    e.addEventListener('click', function () {
+        if (e.checked == false) {
+            checkedBut[i].disabled = true;
+            checkedBut[i].classList.add('disabled')
+        }
+        else {
+            checkedBut[i].disabled = false;
+            checkedBut[i].classList.remove('disabled')
+        }
+    })
+})
+const account__mother = document.querySelector('.account__mother');
+const acount_OK = document.querySelector('.account_OK_mother')
+
+const loginToCabinet = document.querySelectorAll('.loginToCabinet');
+
+loginToCabinet.forEach(e => {
+    e.addEventListener('click', function () {
+        account__mother.classList.add('displayFlex')
+        document.body.classList.add('overflow')
+    })
+})
+account__mother.addEventListener('click', function (e) {
+    if (e.target.classList.contains('displayFlex')) {
+        account__mother.classList.remove('displayFlex')
+        document.body.classList.remove('overflow')
+    }
+})
+
+loginToCabinet[2].onclick = function () {
+    document.body.classList.add('overflow')
+    acount_OK.classList.add('displayFlex')
+
+}
+acount_OK.addEventListener('click', function (e) {
+    if (e.target.classList.contains('displayFlex')) {
+        acount_OK.classList.remove('displayFlex')
+        document.body.classList.remove('overflow')
+    }
+})
+
+const confidation__mother = document.querySelector('.confidation__mother')
+
+const confidation = document.querySelector('.confidation')
+
+confidation.onclick = function () {
+    confidation__mother.classList.add('displayFlex')
+    document.body.classList.add('overflow')
+}
+
+
+confidation__mother.addEventListener('click', function (e) {
+    if (e.target.classList.contains('displayFlex')) {
+        confidation__mother.classList.remove('displayFlex')
+        document.body.classList.remove('overflow')
+    }
 })
