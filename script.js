1afd01d2ae5be901a3c13c9917291224f54f078d
samvelpetr <petrosans05@gmail.com>
2023-10-08 01:53:09 +0400

    comit

--- script.js
+++ script.js
@@ -13,18 +13,34 @@ const swiper1 = new Swiper('.swiper1', {
             return `<span class = "` + className + `">` + `<p>` + `0` + (index + 1) + `</p>` + `</span>`
         }
     },
-    direction: 'vertical',
-    loop: true
+    // direction: 'vertical',
+    loop: true,
 
+    breakpoints: {
+        730: {
+            direction: 'vertical',
+        }
+    }
 })
 
 const swiper2 = new Swiper('.swiper2', {
-    slidesPerView: 3,
+    slidesPerView: 1,
+
     navigation: {
         nextEl: '.swiper-button-next1',
         prevEl: '.swiper-button-prev1',
     },
-    loop: true
+    loop: true,
+
+    breakpoints: {
+        730:{
+            slidesPerView: 2,
+
+        },
+        1350: {
+            slidesPerView: 3,
+        }
+    }
 
 })
 
@@ -76,22 +92,36 @@ const swiper3 = new Swiper('.swiper3', {
 })
 
 const swiper4 = new Swiper('.swiper4', {
-    slidesPerView: 3,
+    slidesPerView: 2,
     navigation: {
         nextEl: '.swiper-button-next3',
         prevEl: '.swiper-button-prev3',
     },
-    spaceBetween: 24,
-    loop: true
+    spaceBetween: 18,
+    loop: true,
+
+    breakpoints: {
+        1350: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+
+        }
+    }
 })
 
 const swiper5 = new Swiper('.swiper5', {
-    slidesPerView: 5,
+    slidesPerView: 3,
     navigation: {
         nextEl: '.swiper-button-next5',
         prevEl: '.swiper-button-prev5',
     },
-    loop: true
+    loop: true,
+
+    breakpoints: {
+        1350: {
+            slidesPerView: 5,
+        }
+    }
 
 })
 
@@ -146,16 +176,54 @@ acount_OK.addEventListener('click', function (e) {
 const confidation__mother = document.querySelector('.confidation__mother')
 
 const confidation = document.querySelector('.confidation')
+const confidation2 = document.querySelector('.confidation2')
 
 confidation.onclick = function () {
     confidation__mother.classList.add('displayFlex')
     document.body.classList.add('overflow')
 }
-
+confidation2.onclick = function () {
+    confidation__mother.classList.add('displayFlex')
+    document.body.classList.add('overflow')
+}
 
 confidation__mother.addEventListener('click', function (e) {
     if (e.target.classList.contains('displayFlex')) {
         confidation__mother.classList.remove('displayFlex')
         document.body.classList.remove('overflow')
     }
+})
+
+
+const sticky__call3__1 = document.querySelector('.sticky__call3__1')
+const sticky__call3__2 = document.querySelector('.sticky__call3__2');
+
+const sticky__call3__open1 = document.querySelector('.sticky__call3__open1')
+const sticky__call3__open2 = document.querySelector('.sticky__call3__open2')
+
+
+sticky__call3__1.addEventListener('click', function () {
+    sticky__call3__1.classList.add('displayNone')
+    sticky__call3__2.classList.add('displayBlock')
+    sticky__call3__open1.classList.add('left1')
+    sticky__call3__open2.classList.add('left2')
+})
+
+sticky__call3__2.addEventListener('click', function () {
+    sticky__call3__1.classList.remove('displayNone')
+    sticky__call3__2.classList.remove('displayBlock')
+    sticky__call3__open1.classList.remove('left1')
+    sticky__call3__open2.classList.remove('left2')
+})
+
+const burgerButton =document.querySelector('.burgerButton')
+
+const burger__menu=document.querySelector('.burger__menu');
+
+burgerButton.addEventListener('click',function(){
+    burger__menu.classList.add('right0')
+})
+const close =document.querySelector('.close')
+close.addEventListener('click',function(){
+    burger__menu.classList.remove('right0')
 })
